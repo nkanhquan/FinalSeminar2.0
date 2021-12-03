3356193bfd825e10d45e12a47d34bf5eb3268848
shroommm <2059035@itec.hcmus.edu.vn>
2021-12-03 11:11:39 +0700

    Create and Change name

--- ExamEditor/ExamEditorForm.cs
+++ ExamEditor/ExamEditorForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace ExamEditor
 {
-    public partial class Form1 : Form
+    public partial class ExamEditor : Form
     {
-        public Form1()
+        public ExamEditor()
         {
             InitializeComponent();
         }
--- ExamEditor/Program.cs
+++ ExamEditor/Program.cs
@@ -16,7 +16,7 @@ namespace ExamEditor
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new ExamEditor());
         }
     }
 }
